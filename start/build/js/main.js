c80fc761ea640b88173b6bf95ae5f865456677f0
PranavU-Coder <pranavu8406@gmail.com>
2025-09-26 22:16:20 +0530

    restarting progress , creating a new domain to host on infinityfree

--- start/build/js/main.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-let username = "pranav";
-console.log(username);
-//# sourceMappingURL=main.js.map
--- start/src/main.ts
+++
@@ -1,2 +0,0 @@
-let username = "pranav"
-console.log(username)
